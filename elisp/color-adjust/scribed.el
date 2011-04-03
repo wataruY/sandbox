3b23427b52f65fdff1629cd9d5de1c12f8493586
Wataru Yamazaki <wataru.yamazaki.nullptr@gmail.com>
2011-04-03 23:30:43 +0900

    Fix LCR algorithm Add expectations

--- elisp/color-adjust/scribed.el
+++ elisp/color-adjust/scribed.el
@@ -1,3 +1,4 @@
+(require 'el-expectations)
 
 (setq exec-path (delete-dups (append (split-string (getenv "PATH") ":" t) exec-path)))
 
@@ -5,24 +6,42 @@
 (frame-parameter nil 'background-color)
 
 (defun wataru:color-luminously-contrast-ratio (c1 c2)
-  (flet ((calc (c) (destructuring-bind (r g b) c
-		     (+ (* r .2126)
-			(* g .7152)
-			(* b .0722)))))
+  "calculate Luminourly Contrast Ratio from two COLORs C1 and C2. For each elements of COLOR (x), x in [0, #xff]."
+  (assert (every (apply-partially 'every
+			   (lambda (x) (and (<= 0 x) (<= x #xff))))
+		 (list c1 c2))
+	  nil
+	  "out of range [0, #xff] : c1 = %s, c2 = %s" c1 c2)
+  (flet ((calc (c) (destructuring-bind (r g b) (mapcar 'wataru:color-rgb-to-linear c)
+		     (+ (* r .2126) (* g .7152) (* b .0722)))))
     (destructuring-bind (l1 l2)
 	(sort (mapcar 'calc (list c1 c2)) '>)
       (/ (+ l1 .05)
 	 (+ l2 .05)))))
 
+(expectations
+  (desc "white:black = 21.0")
+  (expect 21.0 (wataru:color-luminously-contrast-ratio (color-values "black") '(#xff #xff #xff)))
+  (desc "x not in [0,#xff] is error")
+  (expect (error) (wataru:color-luminously-contrast-ratio '(0 0 0) (#xfff #xfff #xfff))))
+(defun wataru:color-color-values (x &optional frame)
+  (mapcar (lambda (x) (lsh x -8)) (color-values x frame)))
+
+(defun* wataru:color-rgb-to-linear (x &optional (maximum #xff))
+  "map monitor RGB color element to linear RGB color element"
+  (let ((sX (/ (* 1.0 x) maximum)))
+    (if (<= sX 0.03928)
+	(/ sX 12.92)
+      (expt (/ (+ sX .055) 1.055) 2.4))))
+(wataru:color-luminously-contrast-ratio (wataru:color-color-values "orchid") (wataru:color-color-values "#001100"))
 
 
-(wataru:color-luminously-contrast-ratio (color-values "#001100") (color-values "white"))
 
 (insert (pp (remove-if (lambda (x) (>= (plist-get x :level) 2))
 		       (mapcar (lambda (x)
 				 (plist-put x
-					    :lcr (wataru:color-luminously-contrast-ratio (color-values (plist-get x :foreground))
-											 (color-values (plist-get x :background))))
+					    :lcr (wataru:color-luminously-contrast-ratio (wataru:color-color-values (plist-get x :foreground))
+											 (wataru:color-color-values (plist-get x :background))))
 				 (plist-put x
 					    :level (let ((lcr (plist-get x :lcr)))
 						     (cond ((< lcr 4.5) 0)
@@ -36,33 +55,40 @@
 									  :background (or (face-background face)
 											  (face-background 'default)) ))))
 					       (face-list)))))))
+(wataru:)
+((:face org-hide :foreground "black" :background "#001100" :lcr 1.0801795217925958 :level 0)
+ (:face which-func :foreground "Blue1" :background "#001100" :lcr 2.2625868669904947 :level 0)
+ (:face custom-invalid :foreground "yellow1" :background "red1" :lcr 3.7235338918507237 :level 0)
+ (:face magit-log-head-label-patches :foreground "IndianRed4" :background "IndianRed1" :lcr 2.721210589780212 :level 0)
+ (:face magit-log-head-label-bisect-bad :foreground "IndianRed4" :background "IndianRed1" :lcr 2.721210589780212 :level 0)
+ (:face magit-log-head-label-bisect-good :foreground "dark olive green" :background "light green" :lcr 4.1973890796378335 :level 0)
+ (:face agda2-highlight-record-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-primitive-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-postulate-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-module-face :foreground "purple" :background "#001100" :lcr 3.6656642642385826 :level 0)
+ (:face agda2-highlight-function-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-datatype-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-coinductive-constructor-face :foreground "gold4" :background "#001100" :lcr 4.29772023926947 :level 0)
+ (:face agda2-highlight-inductive-constructor-face :foreground "green4" :background "#001100" :lcr 4.3446921863527175 :level 0)
+ (:face agda2-highlight-primitive-type-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
+ (:face agda2-highlight-number-face :foreground "purple" :background "#001100" :lcr 3.6656642642385826 :level 0)
+ (:face agda2-highlight-string-face :foreground "firebrick" :background "#001100" :lcr 2.911465218100012 :level 0)
+ (:face anything-emms-playlist :foreground "Springgreen4" :background "#001100" :lcr 4.424247691871591 :level 0)
+ (:face anything-bmkext-man :foreground "Orange4" :background "#001100" :lcr 3.2959884619979203 :level 0)
+ (:face anything-grep-file :foreground "BlueViolet" :background "#001100" :lcr 3.2627936313218933 :level 0)
+ (:face anything-file-name :foreground "Blue" :background "#001100" :lcr 2.2625868669904947 :level 0)
+ (:face navi2ch-article-message-separator-face :foreground "firebrick" :background "#001100" :lcr 2.911465218100012 :level 0)
+ (:face navi2ch-article-auto-decode-face :foreground "gray10" :background "#001100" :lcr 1.1170332673870262 :level 0)
+ (:face ac-yasnippet-selection-face :foreground "white" :background "coral3" :lcr 4.055466747380766 :level 0)
+ (:face ac-selection-face :foreground "white" :background "steelblue" :lcr 4.107556611212924 :level 0)
+ (:face popup-menu-selection-face :foreground "white" :background "steelblue" :lcr 4.107556611212924 :level 0)
+ (:face isearch :foreground "brown4" :background "palevioletred2" :lcr 3.3316816553991817 :level 0))
 
-((:face which-func :foreground "Blue1" :background "#001100" :lcr 1.520168473922962 :level 0)
- (:face coq-solve-tactics-face :foreground "red" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face custom-invalid :foreground "yellow1" :background "red1" :lcr 4.364051897426726 :level 0)
- (:face magit-log-head-label-patches :foreground "IndianRed4" :background "IndianRed1" :lcr 1.830312952309808 :level 0)
- (:face magit-log-head-label-tags :foreground "goldenrod4" :background "LemonChiffon1" :lcr 2.3358513518224937 :level 0)
- (:face magit-log-head-label-bisect-bad :foreground "IndianRed4" :background "IndianRed1" :lcr 1.830312952309808 :level 0)
- (:face magit-log-head-label-bisect-good :foreground "dark olive green" :background "light green" :lcr 2.1555959863158396 :level 0)
- (:face agda2-highlight-error-face :foreground "red" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face agda2-highlight-record-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face agda2-highlight-primitive-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face agda2-highlight-postulate-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face agda2-highlight-function-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face agda2-highlight-datatype-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face agda2-highlight-primitive-type-face :foreground "medium blue" :background "#001100" :lcr 1.2220993738868633 :level 0)
- (:face anything-gentoo-match-face :foreground "red" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face anything-bookmarks-su-face :foreground "red" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face anything-file-name :foreground "Blue" :background "#001100" :lcr 1.520168473922962 :level 0)
- (:face navi2ch-article-auto-decode-face :foreground "gray10" :background "#001100" :lcr 2.1467741249406767 :level 0)
- (:face info-menu-star :foreground "red1" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face ac-yasnippet-selection-face :foreground "white" :background "coral3" :lcr 2.24376810873723 :level 0)
- (:face ac-selection-face :foreground "white" :background "steelblue" :lcr 2.109982160707658 :level 0)
- (:face popup-menu-selection-face :foreground "white" :background "steelblue" :lcr 2.109982160707658 :level 0)
- (:face widget-button-pressed :foreground "red1" :background "#001100" :lcr 4.476254324197864 :level 0)
- (:face isearch :foreground "brown4" :background "palevioletred2" :lcr 2.602284142145173 :level 0)
- (:face header-line :foreground "grey90" :background "grey20" :lcr 4.490182764237567 :level 0)
- (:face mode-line-inactive :foreground "grey80" :background "grey30" :lcr 2.6493464820190966 :level 0))
+
+
+
+
+
 
 
 
