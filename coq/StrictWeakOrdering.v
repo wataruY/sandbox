b917f39164793e21933cb410871960f147c6a676
Wataru Yamazaki <wataru.yamazaki.nullptr@gmail.com>
2011-04-07 18:15:40 +0900

    New coq sto

---
+++ coq/StrictWeakOrdering.v
@@ -0,0 +1,165 @@
+Require Import Relations Setoid.
+
+Section Sort.
+
+Variable (A : Type).
+
+Section Relation.
+  Variable (R : relation A).
+
+  Definition equal := forall x y, ~ R x y -> ~ R y x -> x = y.
+  Definition totality := forall x y, R x y \/ R y x.
+  Definition irreflexive := forall x, ~ R x x.
+  Definition asymmetric := forall x y, R x y -> ~ R y x.
+
+  Theorem asym_imply_irrefl (H : asymmetric) : irreflexive.
+  intro x.
+  intro H'.
+  eapply H; eauto.
+  Qed.
+
+  Theorem asym_imply_antisym (H : asymmetric) : antisymmetric _ R.
+  intros x y H0 H1.
+  contradict H1.
+  apply H.
+  assumption.
+  Qed.
+
+  Theorem antisym_irrefl_assym (Hanti : antisymmetric _ R) (Hirrefl : irreflexive) : asymmetric.
+  intros x y Hxy.
+  intro Hyx.
+  assert (H := Hanti _ _ Hxy Hyx).
+  contradict Hxy.
+  subst.
+  apply Hirrefl.
+  Qed.
+
+  Theorem asym_irrefl_antisym : antisymmetric _ R /\ irreflexive <-> asymmetric.
+  split.
+  intros [ Hanti Hirr ].
+  eapply antisym_irrefl_assym; auto.
+  intro; split.
+  apply asym_imply_antisym; assumption.
+  apply asym_imply_irrefl; assumption.
+  Qed.
+End Relation.
+
+Section Order_Def.
+  Record TotalOrder (R : relation A) :=
+  { to_antisym : antisymmetric _ R
+    ; to_trans : transitive _ R
+    ; to_total : totality R
+  }.
+  Record StrictTotalOrder (R' R : relation A) :=
+  { sto_to : TotalOrder R'
+    ; sto1 x y : R x y <-> R' x y /\ x <> y
+    ; sto_ic x y : R x y <-> ~ R' y x
+  }.
+End Order_Def.
+
+Section Order_Theorems.
+
+Variable (Le Lt : relation A).
+
+Hypotheses (Le_antisym : antisymmetric _ Le)
+           (Le_trans : transitive _ Le)
+           (Le_total : totality Le)
+           (Lt_sto : StrictTotalOrder Le Lt).
+Let Lt_disj := sto1 _ _ Lt_sto.           
+Let Lt_ic := sto_ic _ _ Lt_sto.
+
+Theorem Lt_irrefl : irreflexive Lt.
+intro x.
+intro Hrefl.
+destruct (proj1 (Lt_disj _ _) Hrefl) as [ _ H ].
+contradict H; reflexivity.
+Qed.
+
+Theorem Lt_asymmetric : asymmetric Lt.
+assert (Hanti : antisymmetric A Lt).
+intros x y Hxy Hyx.
+apply Le_antisym; apply Lt_disj; assumption.
+apply asym_irrefl_antisym.
+split; assumption || apply Lt_irrefl.
+Qed.
+
+Theorem Lt_trans : transitive _ Lt.
+intros x y z Hxy Hyz.
+apply Lt_disj.
+split.
+eapply Le_trans.
+apply Lt_disj.
+eauto.
+apply Lt_disj.
+assumption.
+intro.
+contradict Hxy.
+subst.
+apply Lt_asymmetric.
+assumption.
+Qed.
+
+Definition sto_trichotomous_lt x y : x <> y /\ ~ Lt y x <-> Lt x y.
+split.
+intros [Hneq Hlt].
+apply Lt_ic.
+contradict Hlt.
+apply Lt_ic.
+intro H.
+contradict Hneq.
+apply Le_antisym; assumption.
+intro Hlt.
+destruct (proj1 (Lt_disj _ _) Hlt) as [ Hneq Hlt' ].
+split.
+assumption.
+intro.
+apply Lt_ic in H.
+contradiction.
+Qed.
+
+Definition sto_trichitomous_gt x y : x <> y /\ ~ Lt x y <-> Lt y x.
+split.
+intros [Hneq Hgt].
+apply Lt_ic.
+intro.
+contradict Hgt.
+apply Lt_disj; split; assumption.
+intros H.
+split.
+intro Heq.
+contradict H.
+subst.
+apply Lt_irrefl.
+apply Lt_asymmetric.
+assumption.
+Qed.
+
+Require Import Classical.
+
+Theorem not_lt_le x y : ~ Lt y x -> Le x y.
+intro H.
+apply NNPP.
+intro H'.
+eapply Lt_asymmetric.
+apply Lt_ic.
+eauto.
+apply Lt_ic.
+intro H''.
+elim H.
+apply Lt_ic.
+assumption.
+Qed.
+
+Theorem sto_trichitomous_eq x y : ~ Lt y x /\ ~ Lt x y <-> x = y.
+split.
+intro.
+destruct H.
+apply Le_antisym.
+apply not_lt_le.
+assumption.
+apply not_lt_le.
+assumption.
+intro Heq.
+subst.
+split; apply Lt_irrefl.
+Qed.
--- elisp/color-adjust/scribed.el
+++ elisp/color-adjust/scribed.el
@@ -12,18 +12,25 @@
 		 (list c1 c2))
 	  nil
 	  "out of range [0, #xff] : c1 = %s, c2 = %s" c1 c2)
-  (flet ((calc (c) (destructuring-bind (r g b) (mapcar 'wataru:color-rgb-to-linear c)
-		     (+ (* r .2126) (* g .7152) (* b .0722)))))
+  (flet ((calc (c) "Luma(Y') defined in Rec. 709"
+	       (destructuring-bind (r g b) (mapcar 'wataru:color-rgb-to-linear c)
+		 (+ (* r .2126) (* g .7152) (* b .0722)))))
     (destructuring-bind (l1 l2)
 	(sort (mapcar 'calc (list c1 c2)) '>)
       (/ (+ l1 .05)
 	 (+ l2 .05)))))
+
+'(
+  (FG -> BG -> OK + {fg | level3 fg})
+  (\ fg bg -> isLevel3 fg)
+  )
 
 (expectations
   (desc "white:black = 21.0")
   (expect 21.0 (wataru:color-luminously-contrast-ratio (color-values "black") '(#xff #xff #xff)))
   (desc "x not in [0,#xff] is error")
   (expect (error) (wataru:color-luminously-contrast-ratio '(0 0 0) (#xfff #xfff #xfff))))
+
 (defun wataru:color-color-values (x &optional frame)
   (mapcar (lambda (x) (lsh x -8)) (color-values x frame)))
 
@@ -33,6 +40,14 @@
     (if (<= sX 0.03928)
 	(/ sX 12.92)
       (expt (/ (+ sX .055) 1.055) 2.4))))
+
+(defun wataru:color-lcr-level3-p (fg bg)
+  (< 10.0 (wataru:color-luminously-contrast-ratio fg bg)))
+
+;;; find level3 colors 
+(remove-if-not (lambda (x) (wataru:color-lcr-level3-p (wataru:color-color-values x) (wataru:color-color-values "#001100")))
+	       (defined-colors))
+
 (wataru:color-luminously-contrast-ratio (wataru:color-color-values "orchid") (wataru:color-color-values "#001100"))
 
 
@@ -55,34 +70,6 @@
 									  :background (or (face-background face)
 											  (face-background 'default)) ))))
 					       (face-list)))))))
-(wataru:)
-((:face org-hide :foreground "black" :background "#001100" :lcr 1.0801795217925958 :level 0)
- (:face which-func :foreground "Blue1" :background "#001100" :lcr 2.2625868669904947 :level 0)
- (:face custom-invalid :foreground "yellow1" :background "red1" :lcr 3.7235338918507237 :level 0)
- (:face magit-log-head-label-patches :foreground "IndianRed4" :background "IndianRed1" :lcr 2.721210589780212 :level 0)
- (:face magit-log-head-label-bisect-bad :foreground "IndianRed4" :background "IndianRed1" :lcr 2.721210589780212 :level 0)
- (:face magit-log-head-label-bisect-good :foreground "dark olive green" :background "light green" :lcr 4.1973890796378335 :level 0)
- (:face agda2-highlight-record-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-primitive-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-postulate-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-module-face :foreground "purple" :background "#001100" :lcr 3.6656642642385826 :level 0)
- (:face agda2-highlight-function-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-datatype-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-coinductive-constructor-face :foreground "gold4" :background "#001100" :lcr 4.29772023926947 :level 0)
- (:face agda2-highlight-inductive-constructor-face :foreground "green4" :background "#001100" :lcr 4.3446921863527175 :level 0)
- (:face agda2-highlight-primitive-type-face :foreground "medium blue" :background "#001100" :lcr 1.741891571064085 :level 0)
- (:face agda2-highlight-number-face :foreground "purple" :background "#001100" :lcr 3.6656642642385826 :level 0)
- (:face agda2-highlight-string-face :foreground "firebrick" :background "#001100" :lcr 2.911465218100012 :level 0)
- (:face anything-emms-playlist :foreground "Springgreen4" :background "#001100" :lcr 4.424247691871591 :level 0)
- (:face anything-bmkext-man :foreground "Orange4" :background "#001100" :lcr 3.2959884619979203 :level 0)
- (:face anything-grep-file :foreground "BlueViolet" :background "#001100" :lcr 3.2627936313218933 :level 0)
- (:face anything-file-name :foreground "Blue" :background "#001100" :lcr 2.2625868669904947 :level 0)
- (:face navi2ch-article-message-separator-face :foreground "firebrick" :background "#001100" :lcr 2.911465218100012 :level 0)
- (:face navi2ch-article-auto-decode-face :foreground "gray10" :background "#001100" :lcr 1.1170332673870262 :level 0)
- (:face ac-yasnippet-selection-face :foreground "white" :background "coral3" :lcr 4.055466747380766 :level 0)
- (:face ac-selection-face :foreground "white" :background "steelblue" :lcr 4.107556611212924 :level 0)
- (:face popup-menu-selection-face :foreground "white" :background "steelblue" :lcr 4.107556611212924 :level 0)
- (:face isearch :foreground "brown4" :background "palevioletred2" :lcr 3.3316816553991817 :level 0))
 
 
 
